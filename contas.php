022ea7b292e02c9c6fe8ed46a13cd619dd84e200
JoaoPCisilo <joaopedro01032006@gmail.com>
2024-05-03 17:26:10 -0300

    Meu primeiro Commit

--- contas.php
+++ contas.php
@@ -3,9 +3,9 @@
     $num2=1;
 
     $result= $num1 + $num2;
-    $result= $num1 - $num2;
-    $result= $num1 * $num2;
-    $result= $num1 / $num2;
+    $result1= $num1 - $num2;
+    $result2= $num1 * $num2;
+    $result3= $num1 / $num2;
 
-    echo "Os resultados foram: $result";
+    echo "Os resultados foram: $result,$result1,$result2,$result3";
 ?>

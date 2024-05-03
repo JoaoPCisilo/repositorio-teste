7b9ee5d9ff0319c4ae284b0db83500aeef2906b8
JoaoPCisilo <joaopedro01032006@gmail.com>
2024-05-03 17:16:17 -0300

    Atividade 1

---
+++ contas.php
@@ -0,0 +1,11 @@
+<?php
+    $num1=5;
+    $num2=1;
+
+    $result= $num1 + $num2;
+    $result= $num1 - $num2;
+    $result= $num1 * $num2;
+    $result= $num1 / $num2;
+
+    echo "Os resultados foram: $result";
+?>

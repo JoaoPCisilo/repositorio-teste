8107f2e309cb1071f1487a7812ba16544c833043
JoaoPCisilo <joaopedro01032006@gmail.com>
2024-05-10 15:19:29 -0300

    Enviar codigo

--- contas.php
+++ contas.php
@@ -1,11 +1,16 @@
 <?php
-    $num1=5;
-    $num2=1;
+    $num1=10;
+    $num2=4;
+    $num3=8;
+    $num4=20;
+    
 
     $result= $num1 + $num2;
-    $result1= $num1 - $num2;
-    $result2= $num1 * $num2;
-    $result3= $num1 / $num2;
+    $result1= $num4 - $num2;
+    $result2= $num3 * $num2;
+    $result3= $num1 / $num4;
+    $result4= $num3 / ($num1 - $num2);
 
-    echo "Os resultados foram: $result,$result1,$result2,$result3";
+
+    echo "Os resultados foram: $result,$result1,$result2,$result3,$result4";
 ?>
